23281e100ce25c546c58042c6b69625707e60758
Kenta Sato <karupa@cpan.org>
2012-03-10 11:15:52 +0900

    なんか明示的にロードしてあげないとダメっぽい？

--- lib/Tako/Web/Dispatcher.pm
+++ lib/Tako/Web/Dispatcher.pm
@@ -5,6 +5,10 @@ use utf8;
 
 use Amon2::Web::Dispatcher::RouterSimple;
 
+use Tako::Web::C::Root;
+use Tako::Web::C::User;
+use Tako::Web::C::Setting;
+
 connect '/'           => 'Root#index';
 connect '/user/'      => 'User#index';
 connect '/setting/'   => 'Setting#index';
